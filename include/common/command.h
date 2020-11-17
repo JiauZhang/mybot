86d1068b79a5e593bdcdd64e343bce72311fcb81
jiauzhang <1743960454@qq.com>
2020-11-17 21:33:42 +0800

    add FIBOT_CMD macro

--- include/common/command.h
+++ include/common/command.h
@@ -3,12 +3,15 @@
 
 struct cmd_tbl {
 	char *name;
-	char *desc;
-	char *usage;
-	
 	int (*cmd)(struct cmd_tbl *cmd, int argc, char *const argv[]);
+	char *usage;
+	char *help;
 };
 
-#define FIBOT_CMD {}
+#define FIBOT_CMD(__name, __usage, __help, __cmd)                \
+    const struct cmd_tbl __fibot_cmd_##__name                    \
+        __attribute__((unused, section(".fibot_cmd_tbl"))) = {   \
+        #__name, __usage, __help, __cmd                          \
+    }
 
 #endif
--- projects/two-wheeled-robot/robot.c
+++ projects/two-wheeled-robot/robot.c
@@ -2,6 +2,15 @@
 #include <devices/pwm.h>
 #include <common/shell.h>
 #include <common/project.h>
+#include <common/command.h>
+
+int do_test(struct cmd_tbl *cmd, int argc, char *const argv[])
+{
+	prints("test\n");
+	return 0;
+}
+
+FIBOT_CMD(test, do_test, "test", "test");
 
 void start_project()
 {
@@ -12,6 +21,7 @@ void start_project()
 	
 	prints("FiBot> ");
 	while (1) {
+		__fibot_cmd_test.cmd(0, 0, 0);
 		read_line();
 		if (duty) {
 			set_pwm1_duty(1000);
